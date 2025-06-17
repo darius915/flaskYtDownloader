d16a94ce3e102a1859052a5f067915fb7bc25f49
John Darius <71407077+darius915@users.noreply.github.com>
2025-06-17 12:28:09 +0300

    app.py

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ def list_formats():
         ydl_opts = {
             'quiet': True,
             'skip_download': True,
+            'cookiefile': 'cookies.txt',
             'http_headers': {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36'
             }
